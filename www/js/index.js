0aa21c28fc1d0d3d8d1f0ecaf28da6fad0081d9d
jc <guillenz100@gmail.com>
2015-09-11 17:04:01 -0500

    Cambio

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -25,25 +7,34 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
-        
-        
         /*Accediendo a la BD*/
         var db = window.openDatabase('tootlebancrea', '1.0', 'Aplicacion de Negocios Bancrea', 1 * 1024 * 1024);
-        db.transaction(function (tx) {
-          tx.executeSql('CREATE TABLE IF NOT EXISTS tbUsuario (strUsuario unique)');
-          //tx.executeSql("DELETE FROM tbUsuario");
-          //tx.executeSql('INSERT INTO tbUsuario (strUsuario) VALUES ("tootlesoft")');
-          tx.executeSql('SELECT * FROM tbUsuario', [], function (tx, results) {
-            var len = results.rows.length;
-            for (var i = 0; i < len; i++) {
-              alert(results.rows.item(i).strUsuario);
-            }
-          });
-        });
+        db.transaction(populateDB, errorCB, successCB);
         
         $("#pageone").css("display","block");
     }
 };
+
+function populateDB(tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS tbUsuario (strUsuario unique)');
+    tx.executeSql("SELECT * FROM tbUsuario", [], function (tran, table) {
+        if (table.rows.length==0){
+            tx.executeSql('INSERT INTO tbUsuario (strUsuario) VALUES ("tootlesoft")');
+        }else{
+            for (var i = 0; i < table.rows.length; i++) {
+              alert(table.rows.item(i).strUsuario);
+            }
+        }
+    });
+}
+
+function errorCB(err) {
+    alert("Error procesando la consulta SQL: "+err.message);
+}
+
+function successCB() {
+}
+
 
 if(isPhoneGap())
     app.initialize();
