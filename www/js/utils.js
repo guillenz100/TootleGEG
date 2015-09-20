de232ef4d2f7ef838feec6a0e2e384efc9cbf96e
jc <guillenz100@gmail.com>
2015-09-20 01:02:32 -0500

    Cambio

--- www/js/utils.js
+++ www/js/utils.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-var GSERVERURL = "http://www.tootleerp.com/TootleCFDBancrea/TootleCFDUI/Utilerias/Post.aspx?intEmpresa=1&intConsulta=";
+var GSERVERURL = "http://www.tootleerp.com/TootleCFDBancrea/TootleCFDUI/Utilerias/Post.aspx?intEmpresa=1&intSucursal=56&intConsulta=";
 
 
 function parseXML(objData) {
